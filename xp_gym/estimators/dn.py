c36d8a55c4521c2f4e2a4fd234b5c2cc1dfa53e2
Andrew Zheng <azheng92@gmail.com>
2025-10-25 19:25:33 -0500

    Simplify DN

--- xp_gym/estimators/dn.py
+++ xp_gym/estimators/dn.py
@@ -6,19 +6,11 @@ from xp_gym.estimators.network import (
     LimitedMemoryNetworkEstimatorState,
 )
 from xp_gym.observation import Observation
-from xp_gym.estimators.estimator import Estimator, EstimatorState
+from xp_gym.estimators.estimator import Estimator
 from gymnax.environments.environment import Environment, EnvParams
-from jax import Array
-from jaxtyping import Float
 
 
 
-@struct.dataclass
-class DNEstimatorState(EstimatorState):
-    limited_memory_state: LimitedMemoryNetworkEstimatorState
-    sum_rewards: Float[Array, ""]  # For double robust baseline
-
-    
 @struct.dataclass
 class LimitedMemoryDNEstimator(LimitedMemoryNetworkEstimator):
     """
@@ -33,30 +25,28 @@ class LimitedMemoryDNEstimator(LimitedMemoryNetworkEstimator):
 
     def reset(self, rng, env, env_params):
         """Initialize DN v2 estimator with network state."""
-        lms = super().reset(rng, env, env_params)
-        return DNEstimatorState(limited_memory_state=lms, sum_rewards=0.)
+        return super().reset(rng, env, env_params)
 
     def update(
         self,
         env: Environment,
         env_params: EnvParams,
-        state: DNEstimatorState,
+        state: LimitedMemoryNetworkEstimatorState,
         obs: Observation,
     ):
         # Extract observation info
         p = obs.design_info.p
         treatment = obs.action.astype(jnp.float32)
         reward = obs.reward
-        lms = state.limited_memory_state
-        zc = lms.design_cluster_treatments
-        pc = lms.design_cluster_treatment_probs
-        interference_mask = self.interference_mask(env, env_params, lms, obs)
+        zc = state.design_cluster_treatments
+        pc = state.design_cluster_treatment_probs
+        interference_mask = self.interference_mask(env, env_params, state, obs)
 
         z = treatment
         xi = z * (1 - p) / p + (1 - z) * p / (1 - p)
 
         estimate = (
-            lms.estimate
+            state.estimate
             + jnp.sum(
                 interference_mask * (
                     (zc / pc - (1 - zc) / (1 - pc)) * (xi * reward - self.baseline)
@@ -65,18 +55,13 @@ class LimitedMemoryDNEstimator(LimitedMemoryNetworkEstimator):
             + (z / p - (1 - z) / (1 - p)) * (reward - self.baseline)
         )
 
-        new_lms = (
+        return (
             super()
-            .update(env, env_params, lms, obs)
+            .update(env, env_params, state, obs)
             .replace(estimate=estimate)
-        )
-        return DNEstimatorState(
-            limited_memory_state=new_lms,
-            sum_rewards=state.sum_rewards + reward,
         )
 
     def estimate(
-        self, env, env_params, state: DNEstimatorState
+        self, env, env_params, state: LimitedMemoryNetworkEstimatorState
     ):
-        ss = state.limited_memory_state
-        return ss.estimate / ss.t
+        return state.estimate / state.t
